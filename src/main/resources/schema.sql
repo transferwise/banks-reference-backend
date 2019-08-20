6fa671396a5e1811c1a7c796a1ddc6af34df8ea8
Juliano Alves <von.juliano@gmail.com>
2019-08-20 10:48:37 +0100

    Spring generating db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE Credentials(
-    id SERIAL PRIMARY KEY,
-    access_token VARCHAR NOT NULL,
-    refresh_token VARCHAR NOT NULL,
-    expires_in TIMESTAMP
-);
-
-CREATE TABLE Customer(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    credentials_id integer REFERENCES Credentials
-);
-
-INSERT INTO Customer(name) VALUES('John Travolta');
+-- CREATE TABLE Credentials(
+--     id SERIAL PRIMARY KEY,
+--     access_token VARCHAR NOT NULL,
+--     refresh_token VARCHAR NOT NULL,
+--     expires_in TIMESTAMP
+-- );
+--
+-- CREATE TABLE Customer(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255),
+--     credentials_id integer REFERENCES Credentials
+-- );
+--
+-- INSERT INTO Customer(name) VALUES('John Travolta');

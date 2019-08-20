9e8e0fb1ff4c2e330f2a9cdbb4bdc44556b7c86f
Juliano Alves <von.juliano@gmail.com>
2019-08-20 10:49:13 +0100

    currencies resource

---
+++ src/main/java/com/transferwise/t4b/currency/CurrenciesRepository.java
@@ -0,0 +1,6 @@
+package com.transferwise.t4b.currency;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrenciesRepository extends CrudRepository<Currency, String> {
+}

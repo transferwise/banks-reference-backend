cdbfe1c11ef4f33bf5b37b04e59e2691f898a692
Joao Duraes <joao.duraes@transferwise.com>
2019-09-23 17:13:42 +0300

    Add create recipient proxy API

--- src/main/java/com/transferwise/t4b/recipient/RecipientsController.java
+++ src/main/java/com/transferwise/t4b/recipient/RecipientsController.java
@@ -2,11 +2,12 @@ package com.transferwise.t4b.recipient;
 
 import com.transferwise.t4b.customer.CustomersRepository;
 import org.reactivestreams.Publisher;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/recipients")
@@ -26,5 +27,11 @@ public class RecipientsController {
                 .findById(customerId)
                 .map(recipients::all)
                 .orElse(Flux.empty());
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces =  APPLICATION_JSON_VALUE)
+    public Mono<String> create(@RequestParam final Long customerId, final HttpEntity<String> rawRequest) {
+        final var customer = customers.find(customerId);
+        return recipients.create(customer, rawRequest.getBody());
     }
 }
--- src/main/java/com/transferwise/t4b/recipient/TransferWiseRecipients.java
+++ src/main/java/com/transferwise/t4b/recipient/TransferWiseRecipients.java
@@ -8,8 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static com.transferwise.t4b.client.TransferWisePaths.RECIPIENTS_PATH_V2;
-import static com.transferwise.t4b.client.TransferWisePaths.recipientRequirementsPath;
+import static com.transferwise.t4b.client.TransferWisePaths.*;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
@@ -49,6 +48,17 @@ public class TransferWiseRecipients {
         return manager.credentialsFor(customer).flatMap(credentials ->
                 client.post()
                         .uri(recipientRequirementsPath(customer.latestQuoteId()))
+                        .header(AUTHORIZATION, credentials.bearer())
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .body(fromObject(bodyRequest))
+                        .retrieve()
+                        .bodyToMono(String.class));
+    }
+
+    public Mono<String> create(final Customer customer, final String bodyRequest) {
+        return manager.credentialsFor(customer).flatMap(credentials ->
+                client.post()
+                        .uri(RECIPIENTS_PATH_V1)
                         .header(AUTHORIZATION, credentials.bearer())
                         .contentType(APPLICATION_JSON_UTF8)
                         .body(fromObject(bodyRequest))

44dee57094f600ad4dc8e90cacafdf2bd1e3e85f
Joao Duraes <joao.duraes@transferwise.com>
2019-10-03 16:30:43 +0100

    Remove ActivityService to keep consistency with rest of project

--- src/main/java/com/transferwise/t4b/activity/ActivityController.java
+++ src/main/java/com/transferwise/t4b/activity/ActivityController.java
@@ -1,6 +1,8 @@
 package com.transferwise.t4b.activity;
 
+import com.transferwise.t4b.customer.Customer;
 import com.transferwise.t4b.customer.CustomerTransfer;
+import com.transferwise.t4b.customer.CustomersRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +14,16 @@ import java.util.Set;
 @RequestMapping("activities")
 public class ActivityController {
 
-    private final ActivityService activityService;
+    private final CustomersRepository customersRepository;
 
-    public ActivityController(ActivityService activityService) {
-        this.activityService = activityService;
+    public ActivityController(CustomersRepository customersRepository) {
+        this.customersRepository = customersRepository;
     }
 
     @GetMapping
     public Set<CustomerTransfer> getActivities(@RequestParam final Long customerId) {
-        return activityService.getActivities(customerId);
+        Customer customer = customersRepository.find(customerId);
+        return customer.getCustomerTransfers();
     }
 
 }
